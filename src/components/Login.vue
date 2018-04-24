2247121a67c1380f36812f3082b22eb8879889c4
Fxy <fxypooh@163.com>
2018-04-24 12:12:54 +0800

    start axios vuex

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -10,6 +10,8 @@
       <el-button type="warning" >警告按钮</el-button>
       <el-button type="danger" >危险按钮</el-button>
     </el-row>
+    <div>ajax数据：{{data.username}}</div>
+    <div>vuex数据：{{this.$store.state.username}}</div>
   </div>
 </template>
 
@@ -18,13 +20,23 @@ export default {
   name: 'Login',
   data () {
     return {
-      msg: '华迈管理系统V2.0'
+      msg: '华迈管理系统V2.0',
+      data:[]
     }
   },
   methods:{
     routerToDefault(){
       this.$router.push('/home');
     }
+  },
+  created:function(){
+    var _this=this;
+    this.$ajax.get("../../static/data/user.json").then(function(response){
+      //console.log(response.data);
+      _this.data=response.data;
+    }).catch(function(error){
+      console.log(error);
+    });
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,16 +3,25 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//自定义样式主色
 import './assets/styles-element/index.css'
 import ElementUI from 'element-ui'
 //import 'element-ui/lib/theme-chalk/index.css';//默认样式
+import axios from 'axios'
+import Vuex from 'vuex'
+import store from './vuex/store'
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
 Vue.use(ElementUI);
+Vue.use(Vuex);
+Vue.prototype.$ajax = axios;
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
